83a3c91f5ba42279605207cd772cd7fedaaee915
taeh8315 <dreamsky83@naver.com>
2020-07-01 20:27:43 +0900

    Main Loginout

--- app/src/main/java/com/example/kakaotalkclone/MainActivity.java
+++ app/src/main/java/com/example/kakaotalkclone/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.kakaotalkclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
